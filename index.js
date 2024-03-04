4b0c7706b4bad5b9be844a07f4030bda1af8af39
bobbyhadz <opmftw@gmail.com>
2024-03-04 17:19:57 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// EXAMPLE 1 - Concatenate a String with a Variable in JavaScript
+
+const example = 'world';
+
+const result = 'hello ' + example;
+
+console.log(result); // 👉️ 'hello world'
+
+// ------------------------------------------------------------------
+
+// // EXAMPLE 2 - Concatenate a String with a Variable using a template literal
+
+// const example = 'world';
+
+// const result = `hello ${example}`;
+
+// console.log(result); // 👉️ 'hello world'
+
+// ------------------------------------------------------------------
+
+// // EXAMPLE 3 - More examples of using template literals
+
+// const example = 'world';
+
+// const result = `hello ${example}!`;
+// console.log(result); // 👉️ 'hello world!'
+
+// const str = `${50 + 50} percent`;
+// console.log(str); // 👉️ '100 percent'
